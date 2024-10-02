dd20f5b492a3a67dee4089cdc80c5b7c7ee73101
Michał Kaniuk <michal.kaniuk@apius.pl>
2024-10-02 15:01:33 +0200

    Initial commit

---
+++ python-course/libraries/datetime/datetime-tasks-solutions.py
@@ -0,0 +1,48 @@
+"""
+Zadanie 1: Zmiana formatowania daty
+
+Napisz funkcję reformat_date, która przyjmuje datę w formacie MM/DD/YYYY i zwraca ją w formacie YYYY-MM-DD.
+"""
+
+from datetime import datetime
+
+
+def reformat_date(date_str):
+    # Definiowanie oryginalnego formatu daty
+    original_format = "%m/%d/%Y"
+    # Konwersja daty z oryginalnego formatu do obiektu datetime
+    date_obj = datetime.strptime(date_str, original_format)
+    # Definiowanie nowego formatu daty
+    new_format = "%Y-%m-%d"
+    # Formatowanie daty do nowego formatu
+    formatted_date = date_obj.strftime(new_format)
+    return formatted_date
+
+
+# Przykład użycia
+original_date = "08/07/2023"
+formatted_date = reformat_date(original_date)
+print(formatted_date)  # Powinno zwrócić "2023-08-07"
+
+
+"""
+Zadanie 2: Obliczanie wieku w dniach
+
+Uzupełnij funkcję calculate_age, która przyjmuje datę urodzenia w formacie YYYY-MM-DD i zwraca wiek w dniach.
+"""
+
+from datetime import datetime
+
+
+def calculate_age(birthdate_str):
+    birthdate_format = "%Y-%m-%d"
+    birthdate = datetime.strptime(birthdate_str, birthdate_format)
+    today = datetime.today()
+    age_in_days = (today - birthdate).days
+    return age_in_days
+
+
+# Przykład użycia
+birthdate = "1998-05-14"
+age_in_days = calculate_age(birthdate)
+print(age_in_days)  # Zwraca wiek w dniach na podstawie dzisiejszej daty
---
+++ python-course/libraries/os/os-tasks.py
@@ -0,0 +1,13 @@
+"""
+Zadanie 1: Sprawdzanie obecności pliku
+
+
+Napisz funkcję check_file_exists, która przyjmuje ścieżkę do pliku jako argument i zwraca True, jeśli plik istnieje, w przeciwnym razie zwraca False.
+"""
+
+"""
+Zadanie 2: Tworzenie katalogu
+
+
+Napisz funkcję create_directory, która przyjmuje ścieżkę do katalogu jako argument i tworzy ten katalog, jeśli nie istnieje.
+"""

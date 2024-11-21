1c33fe55de63a14264ec9d6ffbbde1d9755fff7b
michal.kaniuk@apius.pl <michal.kaniuk@apius.pl>
2024-11-21 15:21:30 +0000

    Update notebooks

--- 7.libraries/argparse/tasks.py
+++ 7.libraries/argparse/tasks.py
@@ -10,4 +10,6 @@ Opis: Program wypisuje wszystkie liczby od 1 do podanej liczby.
 Zadanie 2: Konwersja temperatury
 
 Opis: Program konwertuje temperaturę z Celsjusza na Fahrenheita lub odwrotnie.
+
+Hint: °F = °C × (9/5) + 32
 '''
